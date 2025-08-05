81f0c657f62186eff83ad1d9a6c9f176da3dd0de
Tanmay Bhatgare <tanmaybhatgare07@gmail.com>
2025-08-05 10:29:57 +0530

    responsive test

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ const App = () => {
     <>
       {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}{" "}
       <div
-        className={`min-h-screen transition-opacity duration-700 ${
+        className={`min-h-screen min-w-screen transition-opacity duration-700 ${
           isLoaded ? "opacity-100" : "opacity-0"
         } bg-black text-gray-100`}
       >
